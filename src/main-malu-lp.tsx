9388f2917739999af01ceed65508bfb4c25bd9ae
rayothechosen <rayothechosen@gmail.com>
2026-08-06 20:17:40 -0300

    chore: remove Malu landing page

--- src/main-malu-lp.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import MaluLanding from "./pages/MaluLanding";
-import "./index.css";
-import "./pages/MaluLanding.css";
-
-createRoot(document.getElementById("root")!).render(<MaluLanding />);
